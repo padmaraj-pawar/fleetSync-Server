620e5d3f27542b1fec1ab991462b0bd8ebce455f
Padmaraj Pawar <padmaraj584@gmail.com>
2026-02-16 16:37:49 +0530

    chore: migrate database connection to Supabase using environment variables

--- database.py
+++ database.py
@@ -1,18 +1,17 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# This tells Python where the database file is
-# Your leader's SQLite file will be named 'tempo.db'
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tempo.db"
+# This loads the variables from your .env file
+load_dotenv()
 
-# Create the engine
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# This pulls the 'DATABASE_URL' we just saved
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# This creates a 'Session' - think of it as a phone line to the DB
+# Create the engine using the secret URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# This is the base class that our data models will inherit from
 Base = declarative_base()
